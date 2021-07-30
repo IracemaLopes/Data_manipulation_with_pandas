c384fb109b64e300c299198a3824ed0292b17404
IracemaLopes <iralopez92@gmail.com>
2021-07-30 16:35:43 +0100

    Added pandas_pivot_table.py

---
+++ pandas_pivot_table.py
@@ -0,0 +1,7 @@
+import pandas as pd
+sales = pd.read_csv("sales_subset.csv")
+# Pivot for mean weekly_sales by store type and holiday
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index="type", columns="is_holiday")
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
